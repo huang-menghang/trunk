749024f824d6eaf9c3bab5f5035f00a065879fdc
huang-menghang <449043209@qq.com>
2017-09-05 17:59:58 +0800

    添加配置以及注解spring-mvc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # trunk
 第一个仓库
+# 基于resulful api 的一个maven 小案例
+get:做展现操作
+post:做添加操作
+put:做修改操作
+delete:做修改操作
+
--- src/main/java/com/yuansheng/resutful/controller/UserController.java
+++ src/main/java/com/yuansheng/resutful/controller/UserController.java
@@ -1,34 +1,45 @@
 package com.yuansheng.resutful.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yuansheng.resultful.core.common.JsonResult;
 
 @Controller
-@RequestMapping(value="/user/*")
+@RequestMapping(value="/users")
 public class UserController {
-    @RequestMapping(value="/user/*",method=RequestMethod.POST,produces="text/json;charset=UTF-8")
+    //列出所有的用户
+	@RequestMapping(value="/",method=RequestMethod.GET,produces="text/html;charset=UTF-8")
+	public ModelAndView editUser(){
+	   ModelAndView view = new ModelAndView("user/index");
+	   return view;
+	}
+	 //添加用户
+    @RequestMapping(value="/",method=RequestMethod.POST,produces="text/json;charset=UTF-8")
 	public String addUser(){
     	JsonResult jsonResult = JsonResult.getInstance(0, "添加成功");
    		return jsonResult.toString();
 	}
-	
-    @RequestMapping(value="/user/*",method=RequestMethod.DELETE,produces="text/json;charset=UTF-8")
-   	public String deleteUser(){
-   		JsonResult jsonResult = JsonResult.getInstance(0, "删除成功");
+	//获取某个用户的信息
+    @RequestMapping(value="/{userId}",method=RequestMethod.GET,produces="text/json;charset=UTF-8")
+   	@ResponseBody
+    public String editUser(@PathVariable("userId")String userId){
+   		JsonResult jsonResult = JsonResult.getInstance(0, "获取用户成功");
    		return jsonResult.toString();
    	}
-    
-    @RequestMapping(value="/user/*",method=RequestMethod.PUT,produces="text/json;charset=UTF-8")
-   	public String updateUser(){
+    //修改某个用户的信息
+    @RequestMapping(value="/{userId}",method=RequestMethod.PUT,produces="text/json;charset=UTF-8")
+   	public String updateUser(@PathVariable("userId")String userId){
     	JsonResult jsonResult = JsonResult.getInstance(0, "修改成功");
    		return jsonResult.toString();
    	}
     
-    @RequestMapping(value="/user/*",method=RequestMethod.GET,produces="text/json;charset=UTF-8")
-   	public String selectUser(){
+    @RequestMapping(value = "/{userId}",method=RequestMethod.DELETE,produces="text/json;charset=UTF-8")
+   	public String deleteUser(@PathVariable("userId")String userId){
     	JsonResult jsonResult = JsonResult.getInstance(0, "查找成功");
    		return jsonResult.toString();
    	}
